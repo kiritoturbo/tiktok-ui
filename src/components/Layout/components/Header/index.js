ec16b9a92d4dba74a651edf712d767a07e001467
kirito <manhtruong@gmail.com>
2023-03-17 15:45:37 +0700

    Dựng khung Layout mặc định

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,5 +1,15 @@
+import classNames from 'classnames/bind';
+import styles from './Header.module.scss'
+
+
+const cx = classNames.bind(styles);//hỗ trợ viết class có dấu gạch ngang kiểu post-item
+
 function Header() {
-    return <h2>Header</h2>;
+    return <header className={cx('wrapper')}>
+        <div className={cx('inner')}>
+            
+        </div>
+    </header>;
 }
 
 export default Header;
