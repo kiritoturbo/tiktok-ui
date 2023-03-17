3c4e8f8a005887c240fe90c96eb5798b00c1a16e
kirito <manhtruong@gmail.com>
2023-03-17 15:08:54 +0700

    Cấu hình Router/Layout cho dự án

---
+++ src/routes/index.js
@@ -0,0 +1,26 @@
+//layout
+import {HeaderOnly} from '~/components/Layout';
+
+import Home from '~/pages/Home'
+import Following from '~/pages/Following'
+import Profile from '~/pages/Profile'
+import Upload from '~/pages/Upload'
+import Search from '~/pages/Search'
+
+
+//những chỗ ko cần đăng nhập vẫn vào được 
+const publicRoutes=[
+    { path:'/',component:Home},
+    { path:'/following',component:Following},
+    { path:'/profile',component:Profile},
+    { path:'/upload',component:Upload , layout:HeaderOnly},
+    { path:'/search',component:Search , layout:null},
+
+]
+
+//phải đăng nhập ms vào được ko thì lái người dùng sang login
+const privateRoutes=[
+
+]
+ 
+export {publicRoutes,privateRoutes}
